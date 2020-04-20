b02d9ccfd4769b330b77cd1dece2a0ee8aa874ee
Ronaldo Vasconcelos <ronaldo.asconcellos@gmail.com>
2020-04-20 20:49:18 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using tabuleiro;
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P = new Posicao(3, 4);
+
+            Console.WriteLine($"Posicao: {P}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ tabuleiro/Tabuleiro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        Peca[,] pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            this.linhas = linhas;
+            this.colunas = colunas;
+            pecas = new Peca[8, 8];
+        }
+    }
+}
